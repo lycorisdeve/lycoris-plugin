d7f70ae81bf5a947db3228dadc9715a55a82e876
lycoris <tanshenghui@sunline.cn>
2023-04-18 00:27:58 +0800

    添加壁纸搜索

--- apps/Wallpaper.js
+++ apps/Wallpaper.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs';
 import { pluginResources } from '../components/lib/Path.js';
-import { getRandomLinkId, getHDWallpaper } from '../model/services/WallpaperService.js'
+import { getRandomLinkId, getHDWallpaper, searchImage } from '../model/services/WallpaperService.js'
+import { segment } from 'icqq';
 
 export class Wallpaper extends plugin {
     constructor() {
@@ -24,16 +25,47 @@ export class Wallpaper extends plugin {
 
     async getWallpaper(e) {
         const link = await getRandomLinkId()
-        if(!link){
+        if (!link) {
             return false
         }
-        const imgInfo= await getHDWallpaper(link)
-        if(imgInfo){
+        const imgInfo = await getHDWallpaper(link)
+        if (imgInfo) {
             e.reply(segment.image(`base64://${imgInfo}`))
         }
-     
+
     }
-   async searchWp(e){
-       
-   }
+    async searchWp(e) {
+        let keyword = e.msg.replace(/#壁纸搜索/g, "").trim()
+        // 翻译中文
+        let chReg = /(?:[\u3400-\u4DB5\u4E00-\u9FEA\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\uD840-\uD868\uD86A-\uD86C\uD86F-\uD872\uD874-\uD879][\uDC00-\uDFFF]|\uD869[\uDC00-\uDED6\uDF00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D\uDC20-\uDFFF]|\uD873[\uDC00-\uDEA1\uDEB0-\uDFFF]|\uD87A[\uDC00-\uDFE0])+/
+        if (chReg.test(keyword)) {
+            e.reply('只支持英文搜索关键词，翻译ing~~~请稍候', true, { recallMsg: 15 })
+            keyword = await trans(keyword)
+            if (chReg.test(keyword)) {
+                return await e.reply("翻译接口寄了，请尝试避免使用中文字符")
+            }
+        }
+        const imgInfos = searchImage(keyword)
+        let msgs = []
+        imgInfos.forEach(img => {
+            const prefix = img.imgName.substring(0, 2);
+            const url = `https://w.wallhaven.cc/full/${prefix}/wallhaven-${img.imgName}`;
+            let msg = `[CQ:image,file=${img.imgUrl}]\n${url}`
+            msgs.push(msg);
+        })
+        e.reply(msgs);
+    }
+}
+async function trans(tg) {
+    let chReg = /([\u3400-\u4DB5\u4E00-\u9FEA\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\uD840-\uD868\uD86A-\uD86C\uD86F-\uD872\uD874-\uD879][\uDC00-\uDFFF]|\uD869[\uDC00-\uDED6\uDF00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D\uDC20-\uDFFF]|\uD873[\uDC00-\uDEA1\uDEB0-\uDFFF]|\uD87A[\uDC00-\uDFE0])+/g
+    // let CH_list = [...Object.values(tg.match(chReg))]
+    let CH_list = tg.match(chReg)
+    if (!CH_list || CH_list.length == 0) { return tg }
+    for (let i = 0; i < CH_list.length; i++) {
+
+        let en = await Translate.t(CH_list[i])
+        tg = tg.replace(CH_list[i], en.toLowerCase())
+
+    }
+    return tg
 }
--- model/services/WallpaperService.js
+++ model/services/WallpaperService.js
@@ -1,44 +1,43 @@
 import * as cheerio from 'cheerio';
 import axios from 'axios';
-import fs from 'node:fs';
-import { pluginResources } from '../../components/lib/Path.js'
+import Translate from '../../utils/translate.js'
 
 export async function getRandomLinkId() {
-    const page = Math.floor(Math.random() * 11000);
-    let url = `https://wallhaven.cc/search?categories=111&purity=010&sorting=favorites&order=desc&ai_art_filter=0&page=${page}`
-    
-    try {
-        // 发送 HTTP 请求获取 HTML 内容
-        const response = await axios.get(url,{
-            timeout:15000
-        });
-        const html = response.data;
+  const page = Math.floor(Math.random() * 11000);
+  let url = `https://wallhaven.cc/search?categories=111&purity=010&sorting=favorites&order=desc&ai_art_filter=0&page=${page}`
 
-        // 使用 Cheerio 解析 HTML 内容，并返回一个解析器函数
-        const $ = cheerio.load(html);
+  try {
+    // 发送 HTTP 请求获取 HTML 内容
+    const response = await axios.get(url, {
+      timeout: 15000
+    });
+    const html = response.data;
 
-            // 获取该 ul 元素下的所有 li 元素
-            const lis = $('figure');
-            
-            if (lis.length > 0) {
-                // 随机选择一个 li 元素
-                const randomLiIndex = Math.floor(Math.random() * lis.length);
-                const randomLi = lis.eq(randomLiIndex);
-                // 获取该 li 元素下的第一个 img 标签的 src 属性
-                const imgUrl = randomLi.find('img').attr('data-src');
+    // 使用 Cheerio 解析 HTML 内容，并返回一个解析器函数
+    const $ = cheerio.load(html);
 
-                // 提取链接中最后一个 '/' 后面的内容作为 ID
-                let imgName = imgUrl.substring(imgUrl.lastIndexOf('/') + 1);
-                // 返回 ID
-                return imgName;
-            } else {
-            logger.info('图片不存在')
-            return false;
-        }
-    } catch (err) {
-        logger.error('An error occurred:', err);
-        return false;
+    // 获取该 ul 元素下的所有 li 元素
+    const lis = $('figure');
+
+    if (lis.length > 0) {
+      // 随机选择一个 li 元素
+      const randomLiIndex = Math.floor(Math.random() * lis.length);
+      const randomLi = lis.eq(randomLiIndex);
+      // 获取该 li 元素下的第一个 img 标签的 src 属性
+      const imgUrl = randomLi.find('img').attr('data-src');
+
+      // 提取链接中最后一个 '/' 后面的内容作为 ID
+      let imgName = imgUrl.substring(imgUrl.lastIndexOf('/') + 1);
+      // 返回 ID
+      return imgName;
+    } else {
+      logger.info('图片不存在')
+      return false;
     }
+  } catch (err) {
+    logger.error('An error occurred:', err);
+    return false;
+  }
 
 }
 
@@ -55,9 +54,9 @@ export async function getHDWallpaper(name) {
         url: url,
         responseType: 'arraybuffer'
       });
-      
+
       const base64Image = Buffer.from(response.data, 'binary').toString('base64');
-      
+
       return base64Image;
     } catch (error) {
       logger.error(error);
@@ -66,4 +65,45 @@ export async function getHDWallpaper(name) {
   }
   return false;
 }
- 
+
+export async function searchImage(keyword) {
+  const url = `https://wallhaven.cc/search?q=${keyword}&categories=111&purity=010&sorting=relevance&order=desc&ai_art_filter=0`
+  try {
+    // 发送 HTTP 请求获取 HTML 内容
+    const response = await axios.get(url, {
+      timeout: 15000
+    });
+    const html = response.data;
+
+    // 使用 Cheerio 解析 HTML 内容，并返回一个解析器函数
+    const $ = cheerio.load(html);
+
+    // 获取所有 <figure> 元素下的 <img> 元素
+    const imgs = $('figure img[data-src]');
+
+    if (imgs.length > 0) {
+      let imgInfos = [];
+      // 遍历每个 <img> 元素，并提取 data-src 属性值作为图片链接
+      imgs.each(function () {
+        const imgUrl = $(this).attr('data-src');
+        if (imgUrl) {
+
+          const imgName = imgUrl.substring(imgUrl.lastIndexOf('/') + 1);
+          let imgInfo = {
+            imgUrl: imgUrl,
+            imgName
+          }
+          imgInfos.push(imgInfo);
+        }
+      });
+      return imgInfos;
+    } else {
+      logger.info('没有找到任何图片');
+      return false;
+    }
+  } catch (err) {
+    logger.error('获取图片列表出错:', err);
+    return false;
+  }
+}
+
