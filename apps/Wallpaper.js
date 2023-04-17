b862431e8ceb3772567c4fdf61405f34ebdbd9d9
lycoris <lycoris.nightmare@gmail.com>
2023-04-17 20:25:44 +0800

    优化bug

--- apps/Wallpaper.js
+++ apps/Wallpaper.js
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+import { pluginResources } from '../components/lib/Path.js';
 import { getRandomLinkId, getHDWallpaper } from '../model/services/WallpaperService.js'
 
 export class Wallpaper extends plugin {
@@ -23,8 +24,9 @@ export class Wallpaper extends plugin {
 
     async getWallpaper(e) {
         let link = await getRandomLinkId()
+        logger.mark(link)
         getHDWallpaper(link)
-        let img = `../../resources/wallpaper/${link}`
+        let img = `${pluginResources}/wallpaper/${link}`
         if (fs.existsSync(img)) {
             e.reply(segment.image(`file://${img}`))
             fs.unlink(filePath, (err) => {
--- model/services/WallpaperService.js
+++ model/services/WallpaperService.js
@@ -1,45 +1,40 @@
 import * as cheerio from 'cheerio';
 import axios from 'axios';
 import fs from 'node:fs';
+import { pluginResources } from '../../components/lib/Path.js'
 
 export async function getRandomLinkId() {
+    const page = Math.floor(Math.random() * 11001);
+    let url = `https://wallhaven.cc/search?categories=111&purity=010&sorting=favorites&order=desc&ai_art_filter=0&page=${page}`
+    
     try {
         // 发送 HTTP 请求获取 HTML 内容
-        const response = await axios.get('https://wallhaven.cc/random');
+        const response = await axios.get(url);
         const html = response.data;
 
         // 使用 Cheerio 解析 HTML 内容，并返回一个解析器函数
         const $ = cheerio.load(html);
 
-        // 获取所有 ul 元素
-        const uls = $('ul');
-
-        if (uls.length > 0) {
-            // 随机选择一个 ul 元素
-            const randomUlIndex = Math.floor(Math.random() * uls.length);
-            const randomUl = uls.eq(randomUlIndex);
-
             // 获取该 ul 元素下的所有 li 元素
-            const lis = randomUl.find('li');
-
+            const lis = $('figure');
+            
             if (lis.length > 0) {
                 // 随机选择一个 li 元素
                 const randomLiIndex = Math.floor(Math.random() * lis.length);
                 const randomLi = lis.eq(randomLiIndex);
-
                 // 获取该 li 元素下的第一个 img 标签的 src 属性
-                const imgUrl = randomLi.find('img').attr('src');
-
+                const imgUrl = randomLi.find('img').attr('data-src');
+                logger.info(imgUrl)
                 // 提取链接中最后一个 '/' 后面的内容作为 ID
                 let imgName = imgUrl.substring(imgUrl.lastIndexOf('/') + 1);
                 // 返回 ID
                 return imgName;
-            }
-        } else {
+            } else {
+            logger.info('图片不存在')
             return false;
         }
     } catch (err) {
-        console.error('An error occurred:', err.message);
+        logger.error('An error occurred:', err);
         return false;
     }
 
@@ -48,9 +43,9 @@ export async function getRandomLinkId() {
 export function getHDWallpaper(name) {
     if (!name) { return false; }
     if (typeof name === 'string') {
-        const prefix = str.substring(0, 2);
+        const prefix = name.substring(0, 2);
         let url = `https://w.wallhaven.cc/full/${prefix}/wallhaven-${name}`
-
+        logger.info(url)
         // 发送 GET 请求获取图片二进制数据
         axios({
             method: 'get',
@@ -58,9 +53,9 @@ export function getHDWallpaper(name) {
             responseType: 'arraybuffer'
         }).then(response => {
             // 将二进制数据写入文件
-            fs.writeFileSync(`../../resources/wallpaper/${name}`, response.data);
+            fs.writeFileSync(`${pluginResources}/wallpaper/${name}`, response.data);
         }).catch(error => {
-            console.log(error);
+            logger.error(error);
         });
 
     }
