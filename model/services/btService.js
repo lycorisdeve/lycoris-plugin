412a2317e3f640ffe0b2ae35124ed365d6d4f9d8
lycoris <tanshenghui@sunline.cn>
2023-04-23 00:13:44 +0800

    修改冲突

--- model/services/btService.js
+++ model/services/btService.js
@@ -4,8 +4,8 @@ import axios from "axios"
 export async function btApi(keyword, pageNum) {
     const http = `http://`
     try {
-        let url = `?w=jie&name=${keyword}&page=${pageNum}`
-        const response = await axios.get(`${http}209.141.34.64:4117/so/clso.php${url}`, {
+        let url = `w=jie&name=${keyword}&page=${pageNum}`
+        const response = await axios.get(`${http}209.141.34.64:4117/so/clso.php?${url}`, {
             timeout: 1500
         });
         if (response.status !== 200) {
@@ -15,12 +15,12 @@ export async function btApi(keyword, pageNum) {
         const data = response.data;
 
         // 将每个元素转换为包含所需字段的对象
-        const result = Array.isArray(data)? data.map(item => ({
-                magnet: 'magnet:?xt=urn:btih:' + item.hash,
-                name: item.name,
-                type: item.type,
-                time: item.time
-            })): [];
+        const result = Array.isArray(data) ? data.map(item => ({
+            magnet: 'magnet:?xt=urn:btih:' + item.hash,
+            name: item.name,
+            type: item.type,
+            time: item.time
+        })) : [];
 
         return result;
 
