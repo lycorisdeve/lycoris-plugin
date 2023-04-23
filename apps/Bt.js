ae47e6c4d8a25fce99b2b925ffe1b9262233320b
lycoris <tanshenghui@sunline.cn>
2023-04-23 15:02:41 +0800

    修改冲突

--- apps/Bt.js
+++ apps/Bt.js
@@ -5,7 +5,7 @@
  */
 import axios from 'axios'
 import * as cheerio from 'cheerio';
-import { btApi } from '../model/services/btService';
+import { btApi } from '../model/services/BtService.js';
 
 /* 
     免责声明
