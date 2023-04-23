3b3bbc47daa7ff2b4738f35c20e150e791433444
lycoris <tanshenghui@sunline.cn>
2023-04-23 14:44:07 +0800

    修改冲突

--- model/services/btService.js
+++ model/services/btService.js
@@ -4,8 +4,8 @@ import axios from "axios"
 export async function btApi(keyword, pageNum) {
     const http = `http://`
     const header = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Accept-Encoding': ' gzip, deflate',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'zh-CN,zh;q=0.9',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
