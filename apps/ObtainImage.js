714d306385207addebded22b79700508d6872dd1
lycoris <lycoris.nightmare@gmail.com>
2024-01-18 17:23:24 +0800

    update

--- apps/ObtainImage.js
+++ apps/ObtainImage.js
@@ -83,9 +83,8 @@ export class Photo extends plugin {
     }
     async pic(e) {
         const url = `https://api.lolimi.cn/API/tup/xjj.php`
-        // let imgUrl = await fetch(url).then(res => res.text()).catch((err) => console.error(err))
-        // console.log(imgUrl)
-        e.reply(segment.image(url))
+        let imgUrl = await fetch(url)
+        e.reply(segment.image(imgUrl))
     }
 
 
