542b8e5b0aee8c29a5c814c60873fe2de1b26e33
lycoris <lycoris.nightmare@gmail.com>
2024-01-18 14:18:32 +0800

    update

---
+++ apps/ImageRecognition.js
@@ -0,0 +1,43 @@
+/**
+ * @Description 图片获取
+ * @author lycoris
+ * @time 2023-03-31 23:57
+ */
+import fetch from 'node-fetch'
+
+
+
+export class Photo extends plugin {
+    constructor() {
+        super({
+            /** 功能名称 */
+            name: '图片识别',
+            /** 功能描述 */
+            dsc: '图片识别',
+            /** https://oicqjs.github.io/oicq/#events */
+            event: 'message',
+            /** 优先级，数字越小等级越高 */
+            priority: 5000,
+            rule: [
+
+                {
+                    /** 命令正则匹配 */
+                    reg: '^#识别',
+                    /** 执行方法 */
+                    fnc: 'picRecognition',
+                },
+
+            ]
+        })
+    }
+
+    async picRecognition(e) {
+        console.log(e.msg)
+
+    }
+
+
+
+
+
+}
