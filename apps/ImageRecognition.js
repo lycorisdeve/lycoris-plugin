9c6ae4b693be47d32ebe75aea526c6fe86fad727
Lycoris <lycorisdeve@qq.com>
2024-02-22 14:15:14 +0800

    update

--- apps/ImageRecognition.js
+++ apps/ImageRecognition.js
@@ -46,7 +46,7 @@ export class Photo extends plugin {
         let data = await fetch(imgUrl).then(res => res.json()).catch((err) => console.error(err))
         data = data.data
         console.log(data)
-        if (data.output != '') {
+        if (data.output != null) {
             e.reply([data.output, segment.image(data.image)])
         } else {
             e.reply("没有找到出处！")
