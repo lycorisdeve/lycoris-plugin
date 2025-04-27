2f8246e77104aab62e419269289b1278d0e29c96
Lycoris <lycorisdeve@qq.com>
2025-04-27 10:43:31 +0800

    update check in

--- apps/OilPrice.js
+++ apps/OilPrice.js
@@ -141,11 +141,11 @@ export class OilPricePlugin extends plugin {
     formatOilPriceInfo(data) {
         return `
 📍 ${data.province} 🚘油价信息
-      ⛽89号汽油：${data.oil89}元/升
-      ⛽92号汽油：${data.oil92}元/升
-      ⛽95号汽油：${data.oil95}元/升
-      ⛽98号汽油：${data.oil98}元/升
-      ⛽0号柴油：${data.oil0}元/升
+⛽89号汽油：${data.oil89}元/升
+⛽92号汽油：${data.oil92}元/升
+⛽95号汽油：${data.oil95}元/升
+⛽98号汽油：${data.oil98}元/升
+⛽0号柴油：${data.oil0}元/升
 ⏰更新时间：${data.updateTime}
         `;
     }
