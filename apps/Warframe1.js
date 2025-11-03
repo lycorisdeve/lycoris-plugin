ccc5ea869c2037bd2b162db0dd37633a65691b69
Lycoris <lycorisdeve@gmail.com>
2025-11-03 15:39:59 +0800

    fix

---
+++ apps/Warframe1.js
@@ -0,0 +1,45 @@
+import fetch from "node-fetch";
+import fs from "node:fs";
+import yaml from "yaml";
+import Config from "../components/Config.js";
+import moment from "moment";
+import puppeteer from "puppeteer";
+import { pluginRootPath } from "../components/lib/Path.js";
+import plugin from "../../../lib/plugins/plugin.js";
+
+const config = Config.getConfig("config");
+let server = config.warframe.server;
+
+let url;
+if (server === "ZHCN") {
+  url = "https://api.null00.com/world/ZHCN/";
+} else if (server === "ZH") {
+  url = "https://api.null00.com/world/ZH/";
+} else {
+  logger.error("warframe插件配置错误，服务器默认设置为国服");
+  url = "https://api.null00.com/world/ZHCN/";
+}
+
+//1.定义命令规则
+export class warframe extends plugin {
+  constructor() {
+    super({
+      /** 功能名称 */
+      name: "warframe",
+      /** 功能描述 */
+      dsc: "warframe信息查询",
+      /** https://oicqjs.github.io/oicq/#events */
+      event: "message",
+      /** 优先级,数字越小等级越高 */
+      priority: 2000,
+      rule: [
+        {
+          /** 命令正则匹配 */
+          reg: "#wf帮助|wfhelp|wf菜单|wf帮助", //匹配消息正则,命令正则
+          /** 执行方法 */
+          fnc: "menu",
+        },
+      ],
+    });
+  }
+}
--- components/lib/Render.js
+++ components/lib/Render.js
@@ -1,50 +1,50 @@
-
-import { Version } from '../Index.js'
-import Config from '../Config.js'
-import { pluginResources, pluginRootPath } from './Path.js'
-import { join } from 'path'
-import puppeteer from '../../../../lib/puppeteer/puppeteer.js'
-
+import { Version } from "../Index.js";
+import Config from "../Config.js";
+import { pluginResources, pluginRootPath } from "./Path.js";
+import { join } from "path";
+import puppeteer from "../../../../lib/puppeteer/puppeteer.js";
 
 async function rednering(path, params, cfg) {
-  let { e } = cfg
+  let { e } = cfg;
   if (!e.runtime) {
-    console.log('未找到e.runtime，请升级至最新版Yunzai')
+    console.log("未找到e.runtime，请升级至最新版Yunzai");
   }
-  return e.runtime.render('lycoris-plugin', path, params, {
-    retType: cfg.retMsgId ? 'msgId' : 'default',
+  return e.runtime.render("lycoris-plugin", path, params, {
+    retType: cfg.retMsgId ? "msgId" : "default",
     beforeRender({ data }) {
-      let resPath = data.pluResPath
-      const layoutPath = `${pluginResources}/common/layout/`
+      let resPath = data.pluResPath;
+      const layoutPath = `${pluginResources}/common/layout/`;
       return {
         ...data,
         _res_path: resPath,
         _layout_path: layoutPath,
         _tpl_path: `${pluginResources}/common/tpl/`,
-        defaultLayout: layoutPath + 'default.html',
-        elemLayout: layoutPath + 'elem.html',
+        defaultLayout: layoutPath + "default.html",
+        elemLayout: layoutPath + "elem.html",
         sys: {
           scale: Cfg.scale(cfg.scale || 1),
-          copyright: `Created By Yunzai-Bot<span class="version">${Version.yunzai}</span> & Lycoris-Plugin<span class="version">${Version.version}</span>`
-        }
-      }
-    }
-  })
+          copyright: `Created By Yunzai-Bot<span class="version">${Version.yunzai}</span> & Lycoris-Plugin<span class="version">${Version.version}</span>`,
+        },
+      };
+    },
+  });
 }
 
-
 function scale(pct = 1) {
-  const scale = Math.min(2, Math.max(0.5, Number(Config.config.renderScale) / 100))
-  pct = pct * scale
-  return `style=transform:scale(${pct})`
+  const scale = Math.min(
+    2,
+    Math.max(0.5, Number(Config.config.renderScale) / 100)
+  );
+  pct = pct * scale;
+  return `style=transform:scale(${pct})`;
 }
 
 async function gitstatus() {
-  const status = await Version.getUpdateStatus()
+  const status = await Version.getUpdateStatus();
   if (status.latest) {
-    return ` SHA: <span class="version">${status.currentCommitId}</span>`
+    return ` SHA: <span class="version">${status.currentCommitId}</span>`;
   } else {
-    return ` SHA: <span class="version">${status.currentCommitId}</span> <span class="tip">(有新版本: ${status.remoteCommitId})</span>`
+    return ` SHA: <span class="version">${status.currentCommitId}</span> <span class="tip">(有新版本: ${status.remoteCommitId})</span>`;
   }
 }
 
@@ -58,30 +58,45 @@ const Render = {
    * @returns
    */
   async render(path, params) {
-    path = path.replace(/.html$/, '')
-    const savePath = '/' + path.replace('html/', '') + '/'
+    path = path.replace(/.html$/, "");
+    const savePath = "/resources/" + path.replace("html/", "") + "/";
     const data = {
-      _res_path: (join(pluginRootPath, '/resources') + '/').replace(/\\/g, '/'),
-      _layout_path: (join(pluginRootPath, '/resources', 'html', 'common', 'layout') + '/').replace(/\\/g, '/'),
-      defaultLayout: (join(pluginRootPath, '/resources', 'html', 'common', 'layout') + '/default.html').replace(/\\/g, '/'),
-      elemLayout: (join(pluginRootPath, '/resources', 'html', 'common', 'layout') + '/elem.html').replace(/\\/g, '/'),
+      _res_path: (join(pluginRootPath, "/resources") + "/").replace(/\\/g, "/"),
+      _layout_path: (
+        join(pluginRootPath, "/resources", "html", "common", "layout") + "/"
+      ).replace(/\\/g, "/"),
+      defaultLayout: (
+        join(pluginRootPath, "/resources", "html", "common", "layout") +
+        "/default.html"
+      ).replace(/\\/g, "/"),
+      elemLayout: (
+        join(pluginRootPath, "/resources", "html", "common", "layout") +
+        "/elem.html"
+      ).replace(/\\/g, "/"),
       sys: {
-        scale: scale(params?.scale || 1)
+        scale: scale(params?.scale || 1),
       },
-      copyright: `${Version.BotName}<span class="version"> v${Version.pluginVersion}</span> & ${Version}<span class="version"> v${Version.version}</span>${await gitstatus()}`,
+      copyright: `${Version.BotName}<span class="version"> v${
+        Version.pluginVersion
+      }</span> & ${Version}<span class="version"> v${
+        Version.version
+      }</span>${await gitstatus()}`,
       pageGotoParams: {
-        waitUntil: 'load'
+        waitUntil: "load",
       },
       tplFile: `${pluginRootPath}/resources/${path}.html`,
       pluResPath: `${pluginRootPath}/resources/`,
-      saveId: path.split('/').pop(),
-      imgType: 'jpeg',
+      saveId: path.split("/").pop(),
+      imgType: "jpeg",
       multiPage: true,
       multiPageHeight: 12000,
-      ...params
-    }
-    return await puppeteer.screenshots(Version.BotName === 'Karin' ? savePath : pluginRootPath + savePath, data)
-  }
-}
+      ...params,
+    };
+    return await puppeteer.screenshots(
+      Version.BotName === "Karin" ? savePath : pluginRootPath + savePath,
+      data
+    );
+  },
+};
 
-export default Render
+export default Render;
