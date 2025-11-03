bc979e3e676e7abcebee1df9141512ae3c22c630
Lycoris <lycorisdeve@gmail.com>
2025-11-03 15:54:04 +0800

    fix

--- apps/Help.js
+++ apps/Help.js
@@ -1,43 +1,45 @@
-import { Version, Render } from '../components/Index.js'
-import HelpService from '../model/HelpService.js'
-
+import { Version, Render } from "../components/Index.js";
+import HelpService from "../model/HelpService.js";
 
 export class Help extends plugin {
-    constructor() {
-        super({
-            name: '彼岸花插件帮助',
-            dsc: '彼岸花插件帮助',
-            event: 'message',
-            priority: 1200,
-            rule: [
-                {
-                    reg: "^#?(ly|lycoris|彼岸花|彼岸|ba)?(命令|帮助|菜单|help|说明|功能|指令|使用说明)$",
-                    fnc: 'help'
-                },
-                {
-                    reg: "^#?彼岸花版本$",
-                    fnc: 'versionInfo'
-                }
-            ]
-        })
-    }
+  constructor() {
+    super({
+      name: "彼岸花插件帮助",
+      dsc: "彼岸花插件帮助",
+      event: "message",
+      priority: 1200,
+      rule: [
+        {
+          reg: "^#?(ly|lycoris|彼岸花|彼岸|ba)?(命令|帮助|菜单|help|说明|功能|指令|使用说明)$",
+          fnc: "help",
+        },
+        {
+          reg: "^#?彼岸花版本$",
+          fnc: "versionInfo",
+        },
+      ],
+    });
+  }
 
-    async help(e) {
+  async help(e) {
+    let data = await HelpService.help(e);
 
-        let data = await HelpService.help(e)
+    let img = Render.render("help/index.html", data, { e, scale: 1.2 });
+    e.reply(img);
+    return;
+  }
 
-        return Render.render(
-            'help/index',
-            data,
-            { e, scale: 1.2 }
-        );
-    }
-
-    async versionInfo(e) {
-        return await Render.render('help/version-info', {
-            currentVersion: Version.version,
-            changelogs: Version.changelogs,
-            elem: 'dendro'
-        }, { e, scale: 1.2 })
-    }
+  async versionInfo(e) {
+    let img = await Render.render(
+      "help/version-info",
+      {
+        currentVersion: Version.version,
+        changelogs: Version.changelogs,
+        elem: "dendro",
+      },
+      { e, scale: 1.2 }
+    );
+    e.reply(img);
+    return;
+  }
 }
